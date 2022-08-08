28ba904dc63f53370deba8b343ccba59653e51dd
Golobon <golobon@ya.ru>
2022-08-08 21:38:21 +0300

    Commit

---
+++ src/Lecture_4/Solution2.java
@@ -0,0 +1,14 @@
+package Lecture_4;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        int big = 1234567890;
+
+        System.out.println(getDifference(big)); // 46 или -46
+    }
+
+    public static float getDifference(int big) {
+        float approx = big;
+        return big - (int) approx;
+    }
+}
