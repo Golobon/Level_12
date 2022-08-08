5e38f3d358b7cf7af0d05daf4994e0eb9fa21461
Golobon <golobon@ya.ru>
2022-08-08 20:50:13 +0300

    Commit

--- src/Lecture_4/Solution.java
+++ src/Lecture_4/Solution.java
@@ -3,11 +3,12 @@ package Lecture_4;
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getSquare(7)); // 49.0
-        System.out.println(getSquare(100_000_001)); // 1.00000002E16
+        System.out.println(getSquare(100_000_00L)); // 1.00000002E16
         System.out.println(getSquare(9_000_000_000_000_000_001L)); // 8.1E37
     }
 
     public static double getSquare(long number) {
-        return (double) number * number;
+        return Math.pow(number, 2);
+
     }
 }
